08548f4d3b855c16cfdb61a8d9b1e544426099fb
Romain Primet <romain.primet@inria.fr>
2017-10-05 09:16:24 +0200

    draft plugin

--- jupyterlab_speads/src/index.ts
+++ jupyterlab_speads/src/index.ts
@@ -1,16 +1,64 @@
 import {
-  JupyterLabPlugin
-} from '@jupyterlab/application';
+  Widget
+} from '@phosphor/widgets';
 
-/**
- * Initialization data for the jupyterlab_speads extension.
- */
-const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_speads',
-  autoStart: true,
-  activate: (app) => {
-    console.log('JupyterLab extension jupyterlab_speads is activated!');
+import {
+  IRenderMime
+} from '@jupyterlab/rendermime-interfaces';
+
+export
+const SPEADS_MIME_TYPE = 'application/vnd.speads+json';
+
+const SPEADS_CLASS = 'jp-RenderedSpeads';
+
+export
+class RenderedSpeads extends Widget implements IRenderMime.IRenderer {
+ 
+  constructor(options: IRenderMime.IRendererOptions) {
+     super();
+
+     this.addClass(SPEADS_CLASS);
+     let vgEmbed = document.createElement('div');
+     vgEmbed.innerHTML = "<i>Placeholder</i> -- conversation plot";
+     this.node.appendChild(vgEmbed);
+     let audio = document.createElement('audio');
+     this.node.appendChild(audio);
   }
+
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    let data = model.data[SPEADS_MIME_TYPE];
+    console.log("speads data", data);
+
+    return Promise.resolve(undefined);
+  }
+}
+
+export
+const rendererFactory: IRenderMime.IRendererFactory = {
+  safe: true,
+  mimeTypes: [SPEADS_MIME_TYPE],
+  createRenderer: options => new RenderedSpeads(options)
+};
+
+const extension: IRenderMime.IExtension = {
+  name: 'speads',
+  rendererFactory,
+  rank: 0,
+  dataType: 'json',
+  documentWidgetFactoryOptions: [{
+    name: 'Spads',
+    modelName: 'text',
+    primaryFileType: 'speads',
+    fileTypes: ['speads', 'json'],
+    defaultFor: ['speads']
+  }],
+  fileTypes: [{
+    mimeTypes: [SPEADS_MIME_TYPE],
+    name: 'speads',
+    fileFormat: 'text',
+    extensions: ['.speads', '.speads.json'],
+    iconClass: 'jp-MaterialIcon jp-VegaIcon',
+  }]
 };
 
 export default extension;
