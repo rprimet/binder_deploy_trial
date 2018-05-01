3601e2c9ff222f234d59f016d107b471aab535eb
Romain Primet <romain.primet@inria.fr>
2018-05-01 16:46:14 +0200

    old plugin superseded by new rendermime-based one

--- jupyterlab_speads/src/typings.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../typings/vega.d.ts"/>
-/// <reference path="../typings/vega-embed.d.ts"/>
-/// <reference path="../typings/vega-util.d.ts"/>
--- jupyterlab_speads/typings/vega-embed.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'vega-embed' {
-    function embed(el: any, spec: any, opt: any): Promise<void>;
-    export = embed;
-}
